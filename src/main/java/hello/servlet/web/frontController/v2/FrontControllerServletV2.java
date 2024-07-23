dbb27c3b51e9229c43d5d34942e8d8ec4bd110f8
lkoblue <gangwon20@gmail.com>
2024-07-23 13:20:49 +0900

    섹션4. mvc 프레임워크 변경 1

---
+++ src/main/java/hello/servlet/web/frontController/v2/FrontControllerServletV2.java
@@ -0,0 +1,43 @@
+package hello.servlet.web.frontController.v2;
+
+import hello.servlet.web.frontController.MyView;
+import hello.servlet.web.frontController.v2.controller.MemberFormControllerV2;
+import hello.servlet.web.frontController.v2.controller.MemberListControllerV2;
+import hello.servlet.web.frontController.v2.controller.MemberSaveControllerV2;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+@WebServlet(name = "frontControllerServletV2", urlPatterns = "/front-controller/v2/*")
+public class FrontControllerServletV2 extends HttpServlet {
+
+    private Map<String, ControllerV2> controllerV2Map = new HashMap<String, ControllerV2>();
+
+    public FrontControllerServletV2() {
+        controllerV2Map.put("/front-controller/v2/members/new-form", new MemberFormControllerV2());
+        controllerV2Map.put("/front-controller/v2/members/save", new MemberSaveControllerV2());
+        controllerV2Map.put("/front-controller/v2/members", new MemberListControllerV2());
+    }
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("FrontControllerServletV2.service");
+
+        String requestURI = request.getRequestURI();
+
+        ControllerV2 controller = controllerV2Map.get(requestURI);
+        if( controller == null ) {
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            return;
+        }
+
+        MyView view = controller.process(request, response);
+        view.render(request, response);
+    }
+}
